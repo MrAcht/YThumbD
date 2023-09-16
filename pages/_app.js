3500c6db10a5b02d5804513e9c6b7455fe512dd3
MrAcht <bkhabache@gmail.com>
2023-09-16 22:55:27 +0100

    Domain

--- pages/_app.js
+++ pages/_app.js
@@ -6,14 +6,14 @@ function MyApp({ Component, pageProps }) {
   return (
     <Fragment>
       <DefaultSeo
-        title="Youtube Thumbnail Downloader"
-        description="Download High-Quality Thumbnails From YouTube Videos"
-        canonical="https://your-website-url.com"
+        title="Download Youtube Thumbnail"
+        description="Download high-quality YouTube and Vimeo thumbnail images for free Our application allows you to easily fetch thumbnails of various qualities Simply paste the video's URL into the input field below and click 'Get Thumbnail Images' to retrieve it"
+        canonical="https://www.youtubethumbnailgrabbing.com/"
         openGraph={{
-          url: "https://your-website-url.com",
-          title: "Youtube Thumbnail Downloader",
-          description: "Download High-Quality Thumbnails From YouTube Videos",
-          site_name: "Youtube Thumbnail Downloader",
+          url: "https://www.youtubethumbnailgrabbing.com/",
+          title: "Download Youtube Thumbnail",
+          description: "Download high-quality YouTube and Vimeo thumbnail images for free Our application allows you to easily fetch thumbnails of various qualities Simply paste the video's URL into the input field below and click 'Get Thumbnail Images' to retrieve it",
+          site_name: "Download Youtube Thumbnail",
         }}
       />
       <Component {...pageProps} />
